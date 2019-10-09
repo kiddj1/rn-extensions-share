6fdd0b430b8132464f27381ae49818a70831414d
kiddj <k.vu0123@gmail.com>
2019-10-09 11:30:08 +0700

    update send multiple for android

--- android/src/main/java/chat/rocket/ShareModule.java
+++ android/src/main/java/chat/rocket/ShareModule.java
@@ -6,10 +6,13 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -63,10 +66,14 @@ public class ShareModule extends ReactContextBaseJavaModule {
 
   public WritableMap processIntent() {
         WritableMap map = Arguments.createMap();
-
+        WritableArray arr = Arguments.createArray();
         String text = "";
         String type = "";
         String action = "";
+        String mimeType = "";
+
+        Context context = getReactApplicationContext();
+        ContentResolver cR = context.getContentResolver();
 
         Activity currentActivity = getCurrentActivity();
 
@@ -86,17 +93,33 @@ public class ShareModule extends ReactContextBaseJavaModule {
             } else if (Intent.ACTION_SEND.equals(action)) {
                 Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
                 if (uri != null) {
+                    WritableMap b = Arguments.createMap();
                     text = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
-                    map.putString("value", text);
-//                    Toast.makeText(getReactApplicationContext(), type, Toast.LENGTH_LONG).show();
-//                    Log.d("STATEVU", type);
+                    b.putString("value", text);
                     if ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) {
                         type = "image/jpeg";
                     } else if (type.equals("video/*")) {
                         type = "video/mp4";
                     }
 
-                    map.putString("type", type);
+                    b.putString("type", type);
+                    arr.pushMap(b);
+                    map.putArray("multiple", arr);
+
+                }
+            } else if(Intent.ACTION_SEND_MULTIPLE.equals(action)) {
+                ArrayList<Uri> fileUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                // Log.d("send multiple", type);
+                if(fileUris != null) {
+                    for(int i = 0; i < fileUris.size() ;i++) {
+                        WritableMap b = Arguments.createMap();
+                        mimeType = cR.getType(fileUris.get(i));
+                        text = "file://" + RealPathUtil.getRealPathFromURI(currentActivity,fileUris.get(i));
+                        b.putString("value", text);
+                        b.putString("type", mimeType);
+                        arr.pushMap(b);
+                    }
+                    map.putArray("multiple", arr);
                 }
             }
         }
